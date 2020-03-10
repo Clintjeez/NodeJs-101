5a2904530a0bdf2f8a4505303ba199ed2962bfa5
clintjeez <clintjeez@gmail.com>
2020-03-10 23:55:01 +0100

    js module patterns

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 var counter = require('./count');
 
-console.log(counter(['shawn', 'crystal', 'ryu']));
+console.log(counter.counter(['shawn', 'crystal', 'ryu']));
+console.log(counter.adder(5,6));
+console.log(counter.adder(counter.pi, 5))
--- count.js
+++ count.js
@@ -2,4 +2,13 @@ var counter = function(arr){
     return 'There are ' + arr.length + 'elements in this array';
 };
 
-module.exports = counter;
+var adder = function(a,b){
+    return `The sum of the 2 numbers is ${a+b}`;
+};
+var pi = 3.142;
+
+module.exports = {
+    counter: counter,
+    adder: adder,
+    pi: pi
+};
