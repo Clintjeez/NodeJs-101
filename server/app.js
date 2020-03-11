60ef68d7e93a3c6e2c2c39bcb107ea9f40f32416
clintjeez <clintjeez@gmail.com>
2020-03-11 13:35:40 +0100

    serving html

--- server/app.js
+++ server/app.js
@@ -11,8 +11,8 @@ myReadStream.on('data', function(chunk){
 
 var server = http.createServer(function(req, res){
    console.log('request was made:' + req.url);
-res.writeHead(200, {'Contnet-Type': 'text/plain'});
-var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+res.writeHead(200, {'Contnet-Type': 'text/html'});
+var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
 //var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
 myReadStream.pipe(res);
 
