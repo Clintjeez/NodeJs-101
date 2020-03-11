830bb303dc0668f59e75678ada2eed2d563ec53c
clintjeez <clintjeez@gmail.com>
2020-03-11 01:35:16 +0100

    the node event emitter

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
-var counter = require('./count');
+//var counter = require('./count');
 
-console.log(counter.counter(['shawn', 'crystal', 'ryu']));
-console.log(counter.adder(5,6));
-console.log(counter.adder(counter.pi, 5))
+//console.log(counter.counter(['shawn', 'crystal', 'ryu']));
+//console.log(counter.adder(5,6));
+//console.log(counter.adder(counter.pi, 5))
+
+//var myEmitter = new events.EventEmitter();
+
+//myEmitter.on('someEvent', function(mssg){
+//    console.log(mssg);
+//});
+
+//myEmitter.emit('someEvent', 'the event was emmitted');
+
+var events = require('events');
+var util = require('util');
+ 
+var Person = function(name){
+    this.name = name;
+}
+
+util.inherits(Person, events.EventEmitter);
+
+var james = new Person('james');
+var mary= new Person('mary');
+var clinton = new Person('clinton');
+var people = [james, mary, clinton];
+
+people.forEach(function(person){
+    person.on('speak', function(mssg){
+        console.log(person.name + ' said: ' + mssg)
+    })
+});
+
+james.emit('speak', 'hey dudes')
+clinton.emit('speak', 'lets go party')
