e123b763b58f59f521adf31b3a4577175e707aa7
clintjeez <clintjeez@gmail.com>
2020-04-05 23:45:27 +0100

    new update

--- server/app.js
+++ server/app.js
@@ -4,11 +4,11 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
- res.sendFile(__dirname + '/index.html');
+ res.render('index');
 });
 
 app.get('/contact', function(req, res){
-    res.sendFile(__dirname + '/contact.html');
+    res.render('contact');
    });
 
 app.get('/profile/:name', function(req, res){
