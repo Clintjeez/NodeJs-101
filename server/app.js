86182d07c12c19f7e234331896ec995e362d154d
clintjeez <clintjeez@gmail.com>
2020-04-11 23:19:02 +0100

    new update

--- server/app.js
+++ server/app.js
@@ -1,14 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
+let urlencodedParser = bodyParser.urlencoded({ extended: false});
+
 app.set('view engine', 'ejs');
+app.use('/assets', express.static('assets'));
 
 app.get('/', function(req, res){
  res.render('index');
 });
 
 app.get('/contact', function(req, res){
-    res.render('contact');
+    res.render('contact', {qs: req.query});
+   });
+
+   app.post('/contact', urlencodedParser, function(req, res){
+    console.log(req.body);
+    res.render('contact-success', {data: req.body});
    });
 
 app.get('/profile/:name', function(req, res){
